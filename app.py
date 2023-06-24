1b0656e609b9123b9b7ab9a16ee0df2293b38b52
Adrian Galicia <adgaben@gmail.com>
2023-06-23 22:44:55 -0600

    Add openai initial configuration

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+import openai
+
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    OPENAI_API_KEY: str = "OPENAI_API_KEY"
+    
+    class Config:
+        env_file = '.env'
+        
+settings = Settings()
+openai.api_key = settings.OPENAI_API_KEY
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
