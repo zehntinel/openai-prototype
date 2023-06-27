b9c9df61dc4caed3a9721486ecc4aa2d09fec1d7
Adrian Galicia <adgaben@gmail.com>
2023-06-26 23:18:01 -0600

    Add pet name generator form backend with openai

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import openai
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
@@ -23,3 +24,22 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request" : request})
 
+@app.post("/", response_class=HTMLResponse)
+async def index(request: Request, animal: str= Form(...)):
+    response = openai.Completion.create(
+        model= "text-davinci-003",
+        prompt= generate_prompt(animal),
+        temperature= 0.6
+    )
+    result = response.choices[0].text
+    return templates.TemplateResponse("index.html", {"request": request, "result": result})
+ 
+def generate_prompt(animal: str):
+    return """Sugiere tres nombres para un animal como si fuera un super heroe.
+
+Animal: Gato
+Names: Capitan MichiBionico, Agente Uñas Afiladas, El felino increible
+Animal: Perro
+Names: Cody La protectora, El canino maravilloso, Super Pachoncito
+Animal: {}
+Names:""".format(animal.capitalize())
